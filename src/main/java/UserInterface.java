1ad6db990049288949d4f8e9b001b0158056ea7d
chri26eq <chri26eq@stud.kea.dk>
2023-09-25 16:06:03 +0200

    go method

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -5,4 +5,31 @@ public class UserInterface {
     public void startProgram() {
 
     }
+
+
+    public void go(String direction) {
+        String choice = scanner.nextLine();
+        switch (choice) {
+            case "n" :
+            case "north" :
+                System.out.println("going north");
+                break;
+            case "e" :
+            case "east" :
+                System.out.println("going east");
+                break;
+            case "s" :
+            case "south" :
+                System.out.println("going south");
+                break;
+            case "w" :
+            case "west" :
+                System.out.println("going west");
+                break;
+            default:
+                System.out.println("invalid input");
+
+
+        }
+    }
 }
