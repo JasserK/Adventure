5bfae68bb776eba7020c949a9e0f6abf13c1f999
ScammedTheTrap <yuay0001@stud.kea.dk>
2023-10-03 15:08:16 +0200

    Arraylist in map changed

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -8,7 +8,6 @@ public class Map {
     public void initializeRooms() {
         ArrayList<Item> itemsInRoom1 = new ArrayList<>();
         itemsInRoom1.add(new Item("Item1", "Item1 description"));
-        itemsInRoom1.add(new Item("Item11", "Item11 description"));
         Room room1 = new Room("Room 1", false,"This is the light description for room 1.", "This is the dark description for room 1.", itemsInRoom1);
 
         ArrayList<Item> itemsInRoom2 = new ArrayList<>();
@@ -22,11 +21,12 @@ public class Map {
         Room room3 = new Room("Room 3", false,"This is the light description for room 3.", "This is the dark description for room 3.", itemsInRoom3);
 
         ArrayList<Item> itemsInRoom4 = new ArrayList<>();
-        //itemsInRoom4.add(new Item("Item4", "Item4 description"));
+        itemsInRoom4.add(new Food("Mario mushroom", "What is this doing here?", 100));
         Room room4 = new Room("Room 4", false,"This is the light description for room 4.", "This is the dark description for room 4.", itemsInRoom4);
 
         ArrayList<Item> itemsInRoom5 = new ArrayList<>();
         itemsInRoom5.add(new Item("Item5", "Item5 description"));
+        itemsInRoom5.add(new Food("Shawarma", "That was a healthy shawarma!", 50));
         Room room5 = new Room("Room 5", false,"This is the light description for room 5.", "This is the dark description for room 5.", itemsInRoom5);
 
         ArrayList<Item> itemsInRoom6 = new ArrayList<>();
@@ -34,7 +34,7 @@ public class Map {
         Room room6 = new Room("Room 6", false,"This is the light description for room 6.", "This is the dark description for room 6.", itemsInRoom6);
 
         ArrayList<Item> itemsInRoom7 = new ArrayList<>();
-        itemsInRoom7.add(new Item("Item7", "Item7 description"));
+        itemsInRoom7.add(new Food("Crystal meth", "Walter White wouldn't be happy about this..", -50));
         Room room7 = new Room("Room 7", false,"This is the light description for room 7.", "This is the dark description for room 7.", itemsInRoom7);
 
         ArrayList<Item> itemsInRoom8 = new ArrayList<>();
@@ -42,7 +42,7 @@ public class Map {
         Room room8 = new Room("Room 8", false,"This is the light description for room 8.", "This is the dark description for room 8.", itemsInRoom8);
 
         ArrayList<Item> itemsInRoom9 = new ArrayList<>();
-        itemsInRoom9.add(new Item("Item9", "Item9 description"));
+        itemsInRoom9.add(new Food("Dirt", "Yuck!", -20));
         Room room9 = new Room("Room 9", false,"This is the light description for room 9.", "This is the dark description for room 9.", itemsInRoom9);
 
         starterRoom = room1;
